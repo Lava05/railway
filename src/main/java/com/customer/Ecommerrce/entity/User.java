c45a8ed0e946c9497430655a7c694c775d11cf5f
Lava05 <168163992+Lava05@users.noreply.github.com>
2024-08-12 18:40:58 +0530

    postgre update

--- src/main/java/com/customer/Ecommerrce/entity/User.java
+++ src/main/java/com/customer/Ecommerrce/entity/User.java
@@ -3,12 +3,14 @@ package com.customer.Ecommerrce.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 import java.util.List;
 
 @Data
 @Entity
+@Table(name = "app_user")
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
